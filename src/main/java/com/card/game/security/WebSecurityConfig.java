dd5344ea17c2abd0f02f0907d542a2b086e6fe5b
Maicon Thales <maiconthales34@hotmail.com>
2022-09-16 13:28:31 -0300

    Alter config.

--- src/main/java/com/card/game/security/WebSecurityConfig.java
+++ src/main/java/com/card/game/security/WebSecurityConfig.java
@@ -55,7 +55,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
-				.antMatchers("/user/auth/**").permitAll()
+				.antMatchers("/login/auth/**").permitAll()
 				.anyRequest()
 				.authenticated();
 
